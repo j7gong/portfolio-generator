918a81ecc96947a14b4e2b7cb0aae05fc5c0dd2d
Jingsi <gongjingsi@hotmail.com>
2022-09-14 22:31:42 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-  console.log('================');
+//   console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `
 };
 
-printProfileData(profileDataArgs);
+// console.log(name, github);
+// console.log(generatePage(name, github));
+const fs = require('fs');
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
