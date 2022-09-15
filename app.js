16dddf8ef754feb43339efcd69b099aed4a6079f
Jingsi <gongjingsi@hotmail.com>
2022-09-14 22:37:14 -0400

    modularize code

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 
@@ -17,29 +26,7 @@ const [name, github] = profileDataArgs;
 
 // printProfileData(profileDataArgs);
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `
-};
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
-const fs = require('fs');
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
