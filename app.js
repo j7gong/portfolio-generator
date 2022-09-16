67b92650adc4fb44119a9935479f45164bb6bf72
Jingsi <gongjingsi@hotmail.com>
2022-09-15 21:48:28 -0400

    add footer, header, and about section

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 
 const promptProject = portfolioData => {
@@ -106,7 +109,13 @@ const promptUser = () => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
     });
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
