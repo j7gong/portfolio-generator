47f38db0a2bbc57ede4a6047b41ca34de394168b
Jingsi <gongjingsi@hotmail.com>
2022-09-15 21:06:10 -0400

    add validation for answers

--- app.js
+++ app.js
@@ -62,7 +62,15 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
@@ -73,6 +81,24 @@ const promptUser = () => {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:'
+          },
+          {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+              if (confirmAbout) {
+                return true;
+              } else {
+                return false;
+              }
+            }
           }
     ]);
 }
